70b1185d438ea46c23e1186dec6deb0f8abe61b4
sukritincs <sukritiverma@northcorp.tech>
2020-08-26 16:30:51 +0530

    Updates

---
+++ precache-manifest.7ae8117bbbdf9f6dbca7d898de208beb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd1470949569fdd6fdfe6123ebd85842",
+    "url": "/Github-Pages/index.html"
+  },
+  {
+    "revision": "7faa4ef1a72cf954fa6d",
+    "url": "/Github-Pages/static/css/2.18fb2ca0.chunk.css"
+  },
+  {
+    "revision": "a8837e6690c5fa6a1367",
+    "url": "/Github-Pages/static/css/main.fec45a73.chunk.css"
+  },
+  {
+    "revision": "7faa4ef1a72cf954fa6d",
+    "url": "/Github-Pages/static/js/2.c8f0806e.chunk.js"
+  },
+  {
+    "revision": "ea717a1fbb69e218c1cb649e92f0d3a8",
+    "url": "/Github-Pages/static/js/2.c8f0806e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8837e6690c5fa6a1367",
+    "url": "/Github-Pages/static/js/main.32f67a65.chunk.js"
+  },
+  {
+    "revision": "5b3b152fa5338450feee",
+    "url": "/Github-Pages/static/js/runtime-main.e91457d1.js"
+  }
+]);
